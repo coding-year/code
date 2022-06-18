d466af94739fcc85a23d567599491d720b406de4
coding-year <107755523+coding-year@users.noreply.github.com>
2022-06-19 00:40:47 +0530

    Add files via upload

---
+++ alpha_cahr3.java
@@ -0,0 +1,30 @@
+/*
+E
+D D
+C C C
+B B B B
+A A A A A
+*/
+
+
+import java.util.*;
+class alpha_cahr3
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int i,j;
+char k='E';
+for(i=1;i<=5;i++)
+{
+
+for(j=1;j<=i;j++)
+{
+System.out.print(k+" ");
+}
+k++;
+System.out.println();
+
+}
+}
+}
